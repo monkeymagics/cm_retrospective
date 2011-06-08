70567a20bc8cd891f2d4222a0d16485b8decf57d
Hiroshi.Nakao <hiroshi.nakao@gmail.com>
2011-06-08 18:00:40 +0900

    簡単なspecを作成

--- spec/cm_retrospective_spec.rb
+++ spec/cm_retrospective_spec.rb
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "CmRetrospective" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  it "最も簡単な使い方" do
+    cmr = CmRetrospective.new('1234567890')
+
+    # 設定ファイルは、カレントディレクトリからルートに向かって検索する
+    # ファイル名は、cm_retrospective.ymlで固定
+    # 設定ファイルには以下の設定が可能。
+    #  * pivotalのトークン
+    #  * デフォルトのペア数
+    #  * デフォルトの1日あたりのローテーション数
+    #  * デフォルトの１イテレーションあたりの作業日数
+
+
+    table = cmr.story_summary_table(:since => 1.week.ago)
+    table.to_text.should == <<EOS
+type     point  spent  story_name
+feature     13     6h  ほげほげほげ
+bug          -     3h  xxxxが動かない
+EOS
+    cmr.velocity(:since => 1.week.ago, :include => :delivered).should == 35
+    cmr.velocity_per_pair_day(:since => 1.week.ago, :include => :delivered, :pair => 3.5).should == 10 # points / pair / day
+    cmr.velocity_per_pair_rotation(:since => 1.week.ago, :include => :delivered, :pair => 3.5).should == 2.5 # points / pair / rotation
   end
 end
