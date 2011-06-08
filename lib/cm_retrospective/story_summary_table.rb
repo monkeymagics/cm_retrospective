199ab496f8691cac051617fb9965fa83efaa581b
Hiroshi.Nakao <hiroshi.nakao@gmail.com>
2011-06-08 18:36:19 +0900

    ポイント実績テーブルをテキストとして出力する CmRetrospective::StorySummaryTable#to_textメソッドのfakeを作りました

--- lib/cm_retrospective/story_summary_table.rb
+++ lib/cm_retrospective/story_summary_table.rb
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
 
 class CmRetrospective
   class StorySummaryTable
+    def to_text
+      "type     point  spent  story_name\nfeature     13     6h  ほげほげほげ\nbug          -     3h  xxxxが動かない\n"
+    end
   end
 end
