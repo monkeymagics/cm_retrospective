93b65b6778af490e82c0500d5810c8620888ed20
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-06-10 15:01:04 +0900

    spec/cm_retrospective_spec.rbにてFakeWebを使用してスタブを作成していた部分をspec/spec_helper.rbに移しました

--- spec/cm_retrospective_spec.rb
+++ spec/cm_retrospective_spec.rb
@@ -5,24 +5,6 @@ require 'fakeweb'
 describe "CmRetrospective" do
 
   before :all do
-    # PivotalTracker::Project
-    project_xml = nil
-    File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/project.xml')) do |f|
-      project_xml = f.read
-    end
-    FakeWeb.register_uri(:get, "http://www.pivotaltracker.com/services/v3/projects/291045",
-      :headers => { 'X-TrackerToken' => "xxxxx", 'Content-Type' => 'application/xml' },
-      :body => project_xml)
-
-    # PivotalTracker::Iteration
-    iterations_xml = nil
-    File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/iterations.xml')) do |f|
-      iterations_xml = f.read
-    end
-    FakeWeb.register_uri(:get, "http://www.pivotaltracker.com/services/v3/projects/291045/iterations/current",
-      :headers => { 'X-TrackerToken' => "xxxxx", 'Content-Type' => 'application/xml' },
-      :body => iterations_xml)
-
     # CmRetrospective.newに渡す設定ファイルのパスは、それを使うコマンドで求めるor入力させる
     # 未入力時には設定ファイルは、カレントディレクトリからルートに向かって検索する
     # デフォルトのファイル名は、cm_retrospective.yml
@@ -94,17 +76,6 @@ EOS
     @cmr.velocity_per_pair_rotation.should == 2.5
     # velocity_per_pair_day:  ストーリーのポイント数の合計 / デフォルトのペア数 / デフォルトの1イテレーションあたりの日数
     @cmr.velocity_per_pair_day(:pair => 3.5, :iteration_days => 4.5).should == 3.0
-  end
-
-  context "story_summary_table" do
-    it "引数に指定した期間のストーリーの2重配列を取得できること" do
-      table = @cmr.story_summary_table(:since => 1.week.ago)
-      table.to_text.should == <<EOS
-type     point  spent  story_name
-feature     13     6h  ほげほげほげ
-bug          -     3h  xxxxが動かない
-EOS
-    end
   end
 
   context "iterations" do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'fake_web'
 require 'cm_retrospective'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -8,5 +9,32 @@ require 'cm_retrospective'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+
 end
+
+# PivotalTracker::Project
+project_xml = nil
+File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/project.xml')) do |f|
+  project_xml = f.read
+end
+FakeWeb.register_uri(:get, "http://www.pivotaltracker.com/services/v3/projects/291045",
+  :headers => { 'X-TrackerToken' => "xxxxx", 'Content-Type' => 'application/xml' },
+  :body => project_xml)
+
+# PivotalTracker::Iteration
+iterations_xml = nil
+File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/iterations.xml')) do |f|
+  iterations_xml = f.read
+end
+FakeWeb.register_uri(:get, "http://www.pivotaltracker.com/services/v3/projects/291045/iterations/current",
+  :headers => { 'X-TrackerToken' => "xxxxx", 'Content-Type' => 'application/xml' },
+  :body => iterations_xml)
+
+# PivotalTracker::Story
+stories_xml = nil
+File.open(File.expand_path(File.dirname(__FILE__) + '/fixtures/stories.xml')) do |f|
+  stories_xml = f.read
+    end
+FakeWeb.register_uri(:get, "http://www.pivotaltracker.com/services/v3/projects/291045/stories",
+  :headers => { 'X-TrackerToken' => "xxxxx", 'Content-Type' => 'application/xml' },
+  :body => stories_xml)
